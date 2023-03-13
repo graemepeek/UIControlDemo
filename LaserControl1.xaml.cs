fce4913258eb2067e61b484b9763058154931387
Graeme Peek <graemepeek@gmail.com>
2023-03-13 19:11:49 -0400

    Azure Push

--- LaserControl1.xaml.cs
+++ LaserControl1.xaml.cs
@@ -20,18 +20,67 @@ namespace UILaserControl
     /// <summary>
     /// Interaction logic for UserControl1.xaml
     /// </summary>
-    public partial class IPCamControl1 : UserControl
+    public partial class LaserControl1 : UserControl
     {
         BAAdsPlcClient _plcClient;
-                
+        private bool _laserenable;
+        private bool _spotweld;
+
 
         public LaserControl1(IBAContainerFacade iocContainer)
         {
             InitializeComponent();
             var adsServer = iocContainer.Resolve<IBAAdsServer>();// get ADSServer
             _plcClient = adsServer.GetAdsClient<IBAAdsPlcClient>("PLC") as BAAdsPlcClient;   // get PLC Client from ADS Server
-          
+            // Get inital states of laser enable and spot weld enable
+            _laserenable = _plcClient.ReadSymbol<bool>("Global_HMI.bLaserEnable");
+            if (_laserenable)
+            {
+                btnLaserEnable.Background = Brushes.Red;
+            }
+            else
+            {
+                btnLaserEnable.Background = Brushes.Green;
+            }
+            _spotweld = _plcClient.ReadSymbol<bool>("Global_HMI.bSpotWeldEnable");
+            if (_spotweld)
+            {
+                btnSpotWeld.Background = Brushes.Red;
+            }
+            else
+            {
+                btnSpotWeld.Background = Brushes.Green;
+            }
         }
 
+        private void btnLaserEnable_Click(object sender, RoutedEventArgs e)
+        {
+            _laserenable = !_laserenable; // Invert laser enable state
+            _plcClient.WriteSymbol("Global_HMI.bLaserEnable", _laserenable);
+            if (_laserenable)
+            {
+                btnLaserEnable.Background = Brushes.Red;
+            }
+            else
+            {
+                btnLaserEnable.Background = Brushes.Green;
+            }
+
+        }
+
+        private void btnSpotWeld_Click(object sender, RoutedEventArgs e)
+        {
+            _spotweld = !_spotweld; // Invert spotweld enable state
+            _plcClient.WriteSymbol("Global_HMI.bSpotWeldEnable", _spotweld);
+            if (_spotweld)
+            {
+                btnSpotWeld.Background = Brushes.Red;
+            }
+            else
+            {
+                btnSpotWeld.Background = Brushes.Green;
+            }
+        }
     }
 }
+
